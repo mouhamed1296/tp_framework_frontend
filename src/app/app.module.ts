0dd3ab70ef5bfefb68db8e6572a67386541da3a6
bmb <bmb>
2022-11-29 13:57:55 +0000

    créationcomposant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminUserComponent } from './admin-user/admin-user.component';
+import { SimpleUserComponent } from './simple-user/simple-user.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminUserComponent,
+    SimpleUserComponent,
+    InscriptionComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule,
